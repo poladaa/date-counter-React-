0683a9d9ce21c7f7b0a3b0dc052c7eb3df8a2f66
luka <120731202+poladaa@users.noreply.github.com>
2023-08-03 16:46:26 +0400

    Add files via upload

---
+++ date-counter/src/App.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handleReset() {
+    setCount(0);
+    setStep(0);
+  }
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <input
+          onChange={(e) => setStep(Number(e.target.value))}
+          type="range"
+          min="1"
+          max="15"
+          value={step}
+        />
+        <span>Step: {step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>RESET</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
